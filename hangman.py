fcb62bd59957702eec6e3d4bbe9d580e3d65402e
SyedSubhan12 <163318679+SyedSubhan12@users.noreply.github.com>
2024-04-04 15:24:49 +0500

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,41 @@
+import random
+import string
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()  # Ensure the word is in uppercase for consistency
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    while len(word_letters) > 0:
+        print("You have used these letters: ", ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+
+        if user_letter in alphabet:
+            if user_letter in used_letters:
+                print('You have already used that character. Please try again!')
+            else:
+                used_letters.add(user_letter)
+                if user_letter in word_letters:
+                    word_letters.remove(user_letter)
+                else:
+                    print("Letter is not in word.")
+        else:
+            print("Invalid character.")
+
+    print(f"Congratulations! The word was '{word}'. You guessed it!")
+
+if __name__ == "__main__":
+    hangman()
+
